9e11e8d9a43ee610c0987428a8a2e30eb4ac5d37
diseptian21 <diseptian5@gmail.com>
2026-05-07 17:51:41 +0700

    import lib

---
+++ src/app/(main)/_components/categori-section.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { motion, AnimatePresence } from "framer-motion";
+
+// Import images from src/images
+import businessImg from "@/images/Business.jpg";
+import lifestyleImg from "@/images/lifestyle.jpg";
+import aiImg from "@/images/AI.jpg";
+import creativityImg from "@/images/Creativity.jpg";
+import educationImg from "@/images/Educationjpg.jpg";
